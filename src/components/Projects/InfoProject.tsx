a2fdc0a16ac791ff6f0f8f33fcb0186a4caeeaf4
Stagiaire ChargeMi <juliizquierdo15@gmail.com>
2022-10-20 13:22:42 +0200

    add link to portfolio

--- src/components/Projects/InfoProject.tsx
+++ src/components/Projects/InfoProject.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
-import { Text, VStack } from '@chakra-ui/react'
+import { Link, Text, VStack } from '@chakra-ui/react'
 import { IInfoProject } from '../../interfaces/IInfoProject'
 
 export default function InfoProject ({ title, description }: IInfoProject) {
   return (
     <VStack
       alignItems={['center', 'center', 'flex-start']}>
-      <Text
+      <Link
+        href='https://portfolio-juliana-izquierdo.vercel.app/'
         fontSize={{ sm: 'md', md: 'xl', lg: '3xl' }}
         fontWeight='600'
         w={'100%'}>
         {title}
-      </Text>
+      </Link>
       <Text>
         {description}
       </Text>
